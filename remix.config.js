4721f1968284641f40132cad53677c9cc8aa5813
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-02 16:27:33 -0400

    needed that

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  serverBuildPath: "build/index.js",
+  publicPath: "/build/",
+  devServerPort: 8002,
+  watchPaths: ["./app"], // Add any custom paths to watch for changes
+};
