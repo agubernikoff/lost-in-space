00f3c325b0d6489e1bdf27799df52364de4ecead
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-06 19:39:27 -0400

    hero

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,6 +1,14 @@
 import { json } from "@remix-run/react";
 import { getSession } from "../sessions";
-import { motion } from "framer-motion";
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import hero from "../assets/images/hero.png";
+import { useRef, useState } from "react";
+import SVGButton from "../components/SVGButton";
 
 export const meta = () => {
   return [
@@ -65,48 +73,81 @@ export default function Index() {
       <motion.div
         animate={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.4, delay: 0.8 }}
+        transition={{ duration: 0.8, delay: 0.4 }}
         className="svg-button-and-words-container"
       >
         <div className="svg-button-container">
-          <SVGButton text={"Let's work together"} />
-          <SVGButton text={"Meet the team"} />
+          <SVGButton
+            text={"Let's work together"}
+            isNavigational={true}
+            path={"/contact"}
+          />
+          <SVGButton
+            text={"Meet the team"}
+            isNavigational={true}
+            path={"/team"}
+          />
         </div>
         <div className="word-container">
           {words.map((word) => (
-            <div className="words">{word}</div>
+            <div key={word} className="words">
+              {word}
+            </div>
           ))}
         </div>
       </motion.div>
+      <HeroContainer />
     </div>
   );
 }
 
-function SVGButton({ text }) {
+function HeroContainer() {
+  const [offset, setOffset] = useState(0);
+  const ref = useRef(null);
+
+  // Get scroll progress of the element
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"], // Element enters and leaves the viewport
+  });
+
+  // Map scroll progress to a value between 0 and 1
+  const visibility = useTransform(scrollYProgress, [0, 1], [0, 20]);
+
+  useMotionValueEvent(visibility, "change", (latest) => {
+    setOffset(latest);
+    console.log(latest);
+  });
+
   return (
-    <motion.button className="svg-button">
-      {text}
-      <svg
-        width="100%"
-        height="100%"
-        viewBox="0 0 100 100"
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-        // className="fourcorners"
-      >
-        {/* top left */}
-        <rect x="0%" y="0%" width="5%" height="3.33%" fill="black" />
-        <rect x="0%" y="0%" width="1%" height="15%" fill="black" />
-        {/* top right */}
-        <rect x="95%" y="0%" width="5%" height="3.33%" fill="black" />
-        <rect x="99%" y="0%" width="1%" height="15%" fill="black" />
-        {/* bottom right */}
-        <rect x="95%" y="96.66%" width="5%" height="3.33%" fill="black" />
-        <rect x="99%" y="85%" width="1%" height="15%" fill="black" />
-        {/* bottom left */}
-        <rect x="0%" y="96.66%" width="5%" height="3.33%" fill="black" />
-        <rect x="0%" y="85%" width="1%" height="15%" fill="black" />
-      </svg>
-    </motion.button>
+    <motion.div
+      ref={ref}
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 100 }}
+      transition={{ duration: 0.4, delay: 1.2 }}
+      className="hero-container"
+    >
+      <div className="crafting-div">
+        <svg
+          width="100"
+          height="100"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          xmlns="http://www.w3.org/2000/svg"
+          // className="fourcorners"
+        >
+          {/* top left */}
+          <rect x="0" y="0" width="10" height="1.65" fill="black" />
+          <rect x="0" y="0" width="1.65" height="10" fill="black" />
+        </svg>
+        <p>CRAFTING EXCEPTIONAL EXPERIENCES FROM START TO FINISH</p>
+        <SVGButton
+          text={"Our Services"}
+          isNavigational={true}
+          path={"/services"}
+        />
+      </div>
+      <motion.img src={hero} alt="" style={{ bottom: `${offset}%` }} />
+    </motion.div>
   );
 }
