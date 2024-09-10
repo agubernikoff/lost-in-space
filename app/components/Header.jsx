5e765a9e532f3be6ad44eb380bc4dd5c0b28f0f5
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-09-10 17:31:49 +0100

    clock, spacing, buttons

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,26 @@
 import { Link } from "@remix-run/react";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
 function Header() {
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const easternTime = new Date().toLocaleTimeString("en-US", {
+        timeZone: "America/New_York",
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      });
+      setTime(easternTime);
+    };
+
+    const intervalId = setInterval(updateTime, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <motion.div
       initial={{ scale: 0.9, y: -50 }}
@@ -13,12 +31,13 @@ function Header() {
       <div className="header-left-and-right">
         <Link to="/">*INSERT LOGO HERE*</Link>
       </div>
-      <div>New York, UTC-4</div>
+      <div>{time}</div> {/* Clock */}
       <div className="header-left-and-right header-right">
         <Link to="/services">Services</Link>
         <Link to="/about">About</Link>
         <Link to="/team">Team</Link>
         <Link to="/contact">Contact</Link>
+        <Link to="/contact">Space Mode</Link>
       </div>
     </motion.div>
   );
