7af6e05c2e9b49d7d578a93644e434e5ba05bd25
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-12 23:24:25 -0400

    add team container to homepage

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -7,6 +7,7 @@ import ClientsContainer from "../components/ClientsContainer";
 import Introduction from "../components/Introduction";
 import VideoOnScroll from "../components/VideoOnScroll";
 import ServicesContainer from "../components/ServicesContainer";
+import TeamContainer from "../components/TeamContainer";
 
 export const meta = () => {
   return [
@@ -23,63 +24,72 @@ export async function loader({ request }) {
 }
 
 export default function Index() {
-  const span = {
-    animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
-    initial: { opacity: 0, y: 100 },
-  };
-
   return (
     <div className="homepage">
-      <div className="home-header">
-        <motion.div
-          initial="initial"
-          animate="animate"
-          transition={{ staggerChildren: 0.4 }}
-        >
-          <div style={{ overflow: "hidden" }}>
-            <motion.span variants={span} className="motion-span">
-              {"BOUTIQUE END-TO-END PRODUCTION "}
-            </motion.span>
-          </div>
-          <div style={{ overflow: "hidden" }}>
-            <motion.span variants={span} className="highlight motion-span">
-              {" & POST-PRODUCTION EXCELLENCE"}
-            </motion.span>
-          </div>
-          <div style={{ overflow: "hidden" }}>
-            <motion.span variants={span} className="motion-span">
-              {" FOR FILMMAKERS AND INDEPENDENT CREATORS."}
-            </motion.span>
-          </div>
-        </motion.div>
-      </div>
-      <motion.div
-        animate={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: 100 }}
-        transition={{ duration: 0.8, delay: 0.4 }}
-        className="svg-button-and-words-container"
-      >
-        <div className="svg-button-container">
-          <SVGButton
-            text={"Let's work together"}
-            isNavigational={true}
-            path={"/contact"}
-          />
-          <SVGButton
-            text={"Meet the team"}
-            isNavigational={true}
-            path={"/team"}
-          />
-        </div>
-      </motion.div>
+      <HomepageHeader />
+      <Buttons />
       <HeroContainer />
       <ClientsContainer />
       <Introduction />
       <VideoOnScroll />
       <ServicesContainer />
-      <p>test</p>
-      <p>test</p>
-      <p>test</p>
+      <TeamContainer />
     </div>
+  );
+}
+
+function HomepageHeader() {
+  const span = {
+    animate: { opacity: 1, y: 0, transition: { duration: 0.4 } },
+    initial: { opacity: 0, y: 100 },
+  };
+  return (
+    <div className="home-header">
+      <motion.div
+        initial="initial"
+        animate="animate"
+        transition={{ staggerChildren: 0.4 }}
+      >
+        <div style={{ overflow: "hidden" }}>
+          <motion.span variants={span} className="motion-span">
+            {"BOUTIQUE END-TO-END PRODUCTION "}
+          </motion.span>
+        </div>
+        <div style={{ overflow: "hidden" }}>
+          <motion.span variants={span} className="highlight motion-span">
+            {" & POST-PRODUCTION EXCELLENCE"}
+          </motion.span>
+        </div>
+        <div style={{ overflow: "hidden" }}>
+          <motion.span variants={span} className="motion-span">
+            {" FOR FILMMAKERS AND INDEPENDENT CREATORS."}
+          </motion.span>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <motion.div
+      animate={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 100 }}
+      transition={{ duration: 0.8, delay: 0.4 }}
+      className="svg-button-and-words-container"
+    >
+      <div className="svg-button-container">
+        <SVGButton
+          text={"Let's work together"}
+          isNavigational={true}
+          path={"/contact"}
+        />
+        <SVGButton
+          text={"Meet the team"}
+          isNavigational={true}
+          path={"/team"}
+        />
+      </div>
+    </motion.div>
   );
 }
