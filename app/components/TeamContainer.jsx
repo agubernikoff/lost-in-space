532dd6ec1807fa5addf1b170288804598e62d066
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-13 00:57:48 -0400

    team container animation

--- app/components/TeamContainer.jsx
+++ app/components/TeamContainer.jsx
@@ -1,6 +1,5 @@
-import { animate, AnimatePresence, motion, useInView } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
-import smoothScroll from "../helpers/SmoothScroll";
+import { motion, useInView } from "framer-motion";
+import React, { useRef, useState } from "react";
 import SVGButton from "./SVGButton";
 import alexImage from "../assets/images/alex.png";
 import caroImage from "../assets/images/caro.png";
@@ -9,7 +8,7 @@ import monishaImage from "../assets/images/monisha.png";
 
 function TeamContainer() {
   const text = useRef(null);
-  const ref = useRef(null);
+  const inView = useInView(text, { once: true, amount: 0.25 });
 
   const [activeModal, setActiveModal] = useState(null);
 
@@ -37,39 +36,7 @@ function TeamContainer() {
   ];
 
   return (
-    <div className="team-container" ref={ref}>
-      <div ref={text} className="centered-team-container">
-        <p>OUR WORK</p>
-        <h2>
-          <div style={{ overflow: "hidden" }}>
-            <span className="motion-span">THE MINDS BEHIND </span>
-          </div>
-          <div style={{ overflow: "hidden" }}>
-            <span className="highlight motion-span">OUR SUCCESS</span>
-          </div>
-        </h2>
-      </div>
-      <div className="team-button">
-        <SVGButton text={"See Everyone"} isNavigational={true} path={"/team"} />
-      </div>
-      <div className="team-members-container">
-        {teamMembers.map((member, index) => (
-          <div key={index} className="team-member">
-            <div className="image-container">
-              <img src={member.image} alt={member.name} />
-              <div
-                className="toggle-button"
-                onClick={() => setActiveModal(index)}
-              >
-                +
-              </div>
-            </div>
-            <div className="team-member-name">{member.name}</div>
-            <div className="team-member-title">{member.title}</div>
-          </div>
-        ))}
-      </div>
-
+    <div className="team-container">
       {activeModal !== null && (
         <>
           <div
@@ -89,6 +56,74 @@ function TeamContainer() {
           </div>
         </>
       )}
+      <div ref={text} className="centered-team-container">
+        <p
+          style={{
+            opacity: inView ? 1 : 0,
+            transform: inView ? "none" : "translateY(100px",
+            transition: "all .4s ease",
+          }}
+        >
+          OUR WORK
+        </p>
+        <h2>
+          <div style={{ overflow: "hidden" }}>
+            <span
+              style={{
+                transform: inView ? "none" : "translateY(100px",
+                transition: "all .4s ease .4s",
+              }}
+              className="motion-span"
+            >
+              THE MINDS BEHIND{" "}
+            </span>
+          </div>
+          <div style={{ overflow: "hidden" }}>
+            <span
+              style={{
+                opacity: inView ? 1 : 0,
+                transform: inView ? "none" : "translateY(100px",
+                transition: "all .4s ease .8s",
+              }}
+              className="highlight motion-span"
+            >
+              OUR SUCCESS
+            </span>
+          </div>
+        </h2>
+      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
+        transition={{ duration: 0.4 }}
+        className="team-button"
+      >
+        <SVGButton text={"See Everyone"} isNavigational={true} path={"/team"} />
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, x: "97vw" }}
+        whileInView={{ opacity: 1, x: 0 }}
+        viewport={{ once: true, amount: 0.005 }}
+        transition={{ duration: 0.8 }}
+        className="team-members-container"
+      >
+        {teamMembers.map((member, index) => (
+          <div key={index} className="team-member">
+            <div className="image-container">
+              <img src={member.image} alt={member.name} />
+              <div
+                className="toggle-button"
+                onClick={() => setActiveModal(index)}
+              >
+                +
+              </div>
+            </div>
+            <div className="team-member-name">{member.name}</div>
+            <div className="team-member-title">{member.title}</div>
+          </div>
+        ))}
+      </motion.div>
     </div>
   );
 }
