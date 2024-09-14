56a55836e19a566209c37e7942dc14a3d5335892
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-14 12:34:48 -0400

    contact form style and minimal animation

--- app/components/ContactFormContainer.jsx
+++ app/components/ContactFormContainer.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useState } from "react";
 import SVGButton from "./SVGButton";
 
@@ -45,67 +46,80 @@ function ContactFormContainer() {
       <p>CONTACT US</p>
       <h2>
         <div style={{ overflow: "hidden" }} />
-        <span className="motion-span">FOR PROJECT INQUIRES, PLEASE</span>
+        <span className="motion-span">
+          <span className="highlight">FOR PROJECT INQUIRES</span>, PLEASE
+        </span>
         <div style={{ overflow: "hidden" }} />
         <span className="motion-span">USE THE CONTACT FORM</span>
       </h2>
       <form onSubmit={handleSubmit}>
-        <input
-          placeholder="First Name"
-          onChange={(e) => setFirstName(e.target.value)}
-          type="text"
-        ></input>
-        <input
-          placeholder="First Name"
-          onChange={(e) => setLastName(e.target.value)}
-          type="text"
-        ></input>
-        <input
-          placeholder="Your Email"
-          onChange={(e) => setEmail(e.target.value)}
-          type="text"
-        ></input>
-        <select
-          defaultValue="placeholder"
-          onChange={(e) => setService(e.target.value)}
-        >
-          <option value="placeholder" disabled hidden>
-            Choose A Service
-          </option>
-          <option value="Post Supervisor Services">
-            Post Supervisor Services
-          </option>
-        </select>
-        <select
-          defaultValue="placeholder"
-          onChange={(e) => setBudget(e.target.value)}
-        >
-          <option value="placeholder" disabled hidden>
-            Choose A Service
-          </option>
-          <option value="$5,000-10,000">$ 5000 - 10000</option>
-        </select>
+        <div className="inputs-container">
+          <input
+            placeholder="First Name"
+            onChange={(e) => setFirstName(e.target.value)}
+            type="text"
+          ></input>
+          <input
+            placeholder="First Name"
+            onChange={(e) => setLastName(e.target.value)}
+            type="text"
+          ></input>
+          <input
+            placeholder="Your Email"
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+          ></input>
+        </div>
+        <div className="selects-container">
+          <select
+            defaultValue="placeholder"
+            onChange={(e) => setService(e.target.value)}
+          >
+            <option value="placeholder" disabled hidden>
+              Choose A Service
+            </option>
+            <option value="Post Supervisor Services">
+              Post Supervisor Services
+            </option>
+          </select>
+          <select
+            defaultValue="placeholder"
+            onChange={(e) => setBudget(e.target.value)}
+          >
+            <option value="placeholder" disabled hidden>
+              Choose A Service
+            </option>
+            <option value="$5,000-10,000">$ 5000 - 10000</option>
+          </select>
+        </div>
         <textarea
           placeholder="Your Message (Optional)"
           onChange={(e) => setMessage(e.target.value)}
+          rows={10}
         ></textarea>
-        <div>
+        <div className="form-button-and-message-container">
           <SVGButton
             isSubmitButton={true}
             text={"Submit form"}
             handleClick={() => {}}
           />
-          {submissionMessage ? (
-            <p
-              className={
-                submissionMessage.error
-                  ? "submission-message error"
-                  : "submission-message"
-              }
-            >
-              {submissionMessage.message}
-            </p>
-          ) : null}
+          <AnimatePresence>
+            {submissionMessage ? (
+              <motion.p
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.4 }}
+                className={
+                  submissionMessage.error
+                    ? "submission-message error"
+                    : "submission-message"
+                }
+              >
+                {submissionMessage.message}
+              </motion.p>
+            ) : null}
+          </AnimatePresence>
         </div>
       </form>
     </div>
