04a8b7b1cb4079e846d2b4b6f333df6c5499e661
alexander gubernikoff <agubernikoff@gmail.com>
2024-09-16 12:09:38 -0400

    imported from figma

--- app/components/SVGCorner.jsx
+++ app/components/SVGCorner.jsx
@@ -2,17 +2,28 @@ import React from "react";
 
 function SVGCorner({ hidden }) {
   return (
+    // <svg
+    //   width="100"
+    //   height="100"
+    //   viewBox="0 0 100 100"
+    //   preserveAspectRatio="none"
+    //   xmlns="http://www.w3.org/2000/svg"
+    //   style={{ display: hidden ? "none" : "block" }}
+    // >
+    //   {/* top left */}
+    //   <rect x="0" y="0" width="100" height="10" fill="black" />
+    //   <rect x="0" y="0" width="10" height="100" fill="black" />
+    // </svg>
     <svg
-      width="100"
-      height="100"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="none"
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      // preserveAspectRatio="xMidYMid meet"
       xmlns="http://www.w3.org/2000/svg"
       style={{ display: hidden ? "none" : "block" }}
     >
-      {/* top left */}
-      <rect x="0" y="0" width="100" height="10" fill="black" />
-      <rect x="0" y="0" width="10" height="100" fill="black" />
+      <path d="M0 0V60H12.4709V12.5606H60V0H0Z" fill="#151515" />
     </svg>
   );
 }
