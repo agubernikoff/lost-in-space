281ce0039ff1961f7e952ff5ece4438253ade122
Sam Gubernikoff <sgubernikoff@gmail.com>
2024-09-16 15:07:16 +0100

    root colors

--- app/routes/services.jsx
+++ app/routes/services.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ServicesContainer from "../components/ServicesContainer";
 import ServicesHero from "../components/ServicesHero";
 
 function services() {
   return (
     <div>
       <ServicesHero />
+      <ServicesContainer />
     </div>
   );
 }
