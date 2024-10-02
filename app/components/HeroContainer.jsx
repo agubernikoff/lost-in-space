a03de66d27655a05a66f657b83b4165688a54727
alexander gubernikoff <agubernikoff@gmail.com>
2024-10-02 12:53:55 -0400

    homepage hero mobile

--- app/components/HeroContainer.jsx
+++ app/components/HeroContainer.jsx
@@ -10,27 +10,14 @@ import SVGButton from "./SVGButton";
 import SVGCorner from "./SVGCorner";
 
 function HeroContainer() {
-  const ref = useRef(null);
-
-  // Get scroll progress of the element
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "end start"], // Element enters and leaves the viewport
-  });
-
-  // Map scroll progress to a value between 0 and 1
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, 180]);
-  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.4, 1, 0.4]);
-
   return (
     <motion.div
-      ref={ref}
       animate={{ opacity: 1, y: 0 }}
       initial={{ opacity: 0, y: 100 }}
       transition={{ duration: 0.4, delay: 1.2 }}
       className="hero-container"
     >
-      <motion.img src={hero} alt="" style={{ rotate, scale }} />
+      <img src={hero} alt="" />
       <div className="crafting-div">
         <SVGCorner />
         <p>CRAFTING EXCEPTIONAL EXPERIENCES FROM START TO FINISH</p>
